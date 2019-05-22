6c0a64df1c4e68c2b97479186da39d4d7d42a1f5
jspiner <jspiner@naver.com>
2019-05-22 20:48:31 +0900

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by 성 미니 on 22/05/2019.
+//  Copyright © 2019 성 미니. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ calculatorTests/calculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  calculatorTests.swift
+//  calculatorTests
+//
+//  Created by 성 미니 on 22/05/2019.
+//  Copyright © 2019 성 미니. All rights reserved.
+//
+
+import XCTest
+@testable import calculator
+
+class calculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
